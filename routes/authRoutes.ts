deef97dd403a85783065b4b7a3747c3a4e7bba2f
Mikkel <mikk284p@edu.sde.dk>
2024-11-07 08:31:27 +0100

    Feature: tilføjet controllers til vores use-cases

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,11 +1,13 @@
 // routes/authRoutes.js
 import express from "express";
 import { signup, signin, refreshToken } from "../controllers/authController";
+import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/refresh-token", refreshToken);
+
 
 export default router;

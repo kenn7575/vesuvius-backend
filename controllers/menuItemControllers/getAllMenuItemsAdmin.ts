d3638130637be64389681227533a8c8b499cec99
kenni kollemorten <kenn7575@gmail.com>
2024-12-08 01:04:10 +0100

    feat(menuItems): enhance admin menu item retrieval with pagination and filtering

--- controllers/menuItemControllers/getAllMenuItemsAdmin.ts
+++ controllers/menuItemControllers/getAllMenuItemsAdmin.ts
@@ -1,40 +1,82 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { idSchema } from "../../zodSchemas/schemas";
 
 const prisma = new PrismaClient();
 
-export async function getAllMenuItems(
+const schema = z.object({
+  item_type_id: idSchema.optional(),
+  filter: z.enum(["active", "inactive", "all"]).optional(),
+  page: z.number().int().positive().optional(),
+  perPage: z.number().int().positive().optional(),
+});
+
+export async function getAllMenuItemsAdmin(
   req: Request,
   res: Response
 ): Promise<void | any> {
   try {
     const item_type_id: string = req.query.item_type_id as string;
     const isActiveFilter: string = req.query.filter as string;
+    const page: number = parseInt(req.query.page as string) || 1;
+    const perPage: number = parseInt(req.query.perPage as string) || 10;
+
+    const result = schema.safeParse({
+      item_type_id,
+      filter: isActiveFilter,
+      page,
+      perPage,
+    });
+
+    if (!result.success) {
+      return res
+        .status(400)
+        .json({ message: result.error.flatten().fieldErrors });
+    }
 
     let whereClause: any = {};
 
-    // Handle is_active filter
-    if (isActiveFilter === "Active") {
+    if (result.data.filter === "active") {
       whereClause.is_active = true;
-    } else if (isActiveFilter === "Inactive") {
+    } else if (result.data.filter === "inactive") {
       whereClause.is_active = false;
     }
-    // If isActiveFilter is "All" or undefined, no filter will be applied
 
-    // Add type_id filter if present
-    if (parseInt(item_type_id)) {
-      whereClause.type_id = parseInt(item_type_id);
+    if (result.data.item_type_id) {
+      whereClause.type_id = result.data.item_type_id;
     }
 
-    const menuItems = await prisma.menu_item.findMany({
-      where: whereClause,
-    });
+    const skip = (page - 1) * perPage;
 
-    res.json(menuItems);
+    const [menuItems, total, menuItemTypes] = await Promise.all([
+      prisma.menu_item.findMany({
+        where: whereClause,
+        skip,
+        take: perPage,
+      }),
+      prisma.menu_item.count({
+        where: whereClause,
+      }),
+      prisma.menu_item_types.findMany(),
+    ]);
+
+    res.json({
+      items: menuItems.map((item) => ({
+        ...item,
+        type: menuItemTypes.find((type) => type.id === item.type_id)?.name,
+      })),
+      meta: {
+        total,
+        page,
+        perPage,
+        pageCount: Math.ceil(total / perPage),
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
 }
 
-export default getAllMenuItems;
+export default getAllMenuItemsAdmin;
--- routes/menuItemRoutes.ts
+++ routes/menuItemRoutes.ts
@@ -6,12 +6,14 @@ import getMenuItem from "../controllers/menuItemControllers/getMenuItem";
 import getAllMenuItems from "../controllers/menuItemControllers/getAllMenuItems";
 import deactivateMenuItem from "../controllers/menuItemControllers/deactivateMenuItem";
 import activateMenuItem from "../controllers/menuItemControllers/activateMenuItem";
+import getAllMenuItemsAdmin from "../controllers/menuItemControllers/getAllMenuItemsAdmin";
 
 const router = express.Router();
 
 // all routes in this file are protected by the authenticateToken middleware
 // all routes in this file are prefixed with /menu_items
 
+router.get("/admin", authMiddleware, getAllMenuItemsAdmin); //get all for admins
 router.post("/", authMiddleware, createMenuItem); //create
 router.put("/:id", authMiddleware, updateMenuItem); //update specific
 router.delete("/:id/deactivate", authMiddleware, deactivateMenuItem); //delete specific
